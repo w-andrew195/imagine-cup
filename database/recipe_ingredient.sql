81061546e2e21b71cea2cfd58213b9046d28f6b0
cam <cameronlh.smith@outlook.com>
2018-03-22 15:33:35 +1300

    Translate SQL into Transact-SQL dialect

--- database/recipe_ingredient.sql
+++ database/recipe_ingredient.sql
@@ -1,26 +1,19 @@
-/* This file creates the basic tables for recipies and their constituent
- * ingredients, then populates them with sample data
- * 
- * Usage:       sqlite3 database_file.db
-            ->  .read recipe_ingredient.sql
-*/
-
-/* Delete existing tables incase a .db file already exists */
+/* Must drop RecipeIngredient first because of the FK constraints */
+DROP TABLE IF EXISTS RecipeIngredient;
 DROP TABLE IF EXISTS Recipe;
 DROP TABLE IF EXISTS Ingredient;
-DROP TABLE IF EXISTS RecipeIngredient;
 
 /* Create main recipe table */
-CREATE TABLE IF NOT EXISTS Recipe (
-    RECIPE_ID INTEGER NOT NULL PRIMARY KEY,
+CREATE TABLE Recipe (
+    RECIPE_ID INTEGER NOT NULL PRIMARY KEY IDENTITY,
     NAME TEXT,
     DESCRIPTION TEXT,
     INSTRUCTIONS TEXT
 );
 
 /* Create main ingredient table */
-CREATE TABLE IF NOT EXISTS Ingredient (
-    INGREDIENT_ID INTEGER NOT NULL PRIMARY KEY,
+CREATE TABLE Ingredient (
+    INGREDIENT_ID INTEGER NOT NULL PRIMARY KEY IDENTITY,
     NAME TEXT,
     DESCRIPTION TEXT,
     CATEGORY TEXT,
@@ -33,13 +26,12 @@ CREATE TABLE IF NOT EXISTS Ingredient (
 
 /* Create associative entity set for the many to many relationship between
  * Recipe and Ingredients */
-CREATE TABLE IF NOT EXISTS RecipeIngredient (
-    INGREDIENT_ID INTEGER NOT NULL,
-    RECIPE_ID INTEGER NOT NULL,
+CREATE TABLE RecipeIngredient (
+    /* strange MS syntax... */
+    INGREDIENT_ID INTEGER NOT NULL CONSTRAINT FK_INGREDIENT_ID FOREIGN KEY REFERENCES Ingredient(INGREDIENT_ID),
+    RECIPE_ID INTEGER NOT NULL CONSTRAINT FK_RECIPE_ID FOREIGN KEY REFERENCES Recipe(RECIPE_ID),
     AMOUNT INTEGER,
-    PRIMARY KEY (INGREDIENT_ID, RECIPE_ID),
-    FOREIGN KEY (INGREDIENT_ID) REFERENCES Ingredient (INGREDIENT_ID),
-    FOREIGN KEY (RECIPE_ID) REFERENCES Ingredient (RECIPE_ID)
+    PRIMARY KEY (INGREDIENT_ID, RECIPE_ID)
 );
 
 /* Insert dummy data */
@@ -47,9 +39,9 @@ INSERT INTO Recipe (
     NAME,
     DESCRIPTION
 ) VALUES
-("Chocolate Cake", "A delicious chocolate cake"),
-("Chicken Quesidillas", "Spicy chicken quesidillas with gluten-free "),
-("Sweet and Sour Pork", "Its both sweet and sour...");
+('Chocolate Cake', 'A delicious chocolate cake'),
+('Chicken Quesidillas', 'Spicy chicken quesidillas with gluten-free wrap'),
+('Sweet and Sour Pork', 'Its both sweet and sour...');
 
 INSERT INTO Ingredient (
     NAME,
@@ -60,9 +52,9 @@ INSERT INTO Ingredient (
     FAT,
     CALORIES
 ) VALUES 
-("Pork, lean", "Lean pork cuts", "MEAT", 27, 0, 14, 242),
-("Chicken, breast", "Raw chicken breast", "MEAT", 23, 0, 1.2, 110),
-("Milk Chocolate", "Milk Chocolate", "SWEETS", 7.7, 59, 30, 535);
+('Pork, lean', 'Lean pork cuts', 'MEAT', 27, 0, 14, 242),
+('Chicken, breast', 'Raw chicken breast', 'MEAT', 23, 0, 1.2, 110),
+('Milk Chocolate', 'Milk Chocolate', 'SWEETS', 7.7, 59, 30, 535);
 
 INSERT INTO RecipeIngredient (
     INGREDIENT_ID,
@@ -70,17 +62,17 @@ INSERT INTO RecipeIngredient (
     AMOUNT
 ) VALUES
 (
-    (SELECT INGREDIENT_ID FROM Ingredient WHERE NAME LIKE "Pork, lean"),
-    (SELECT RECIPE_ID FROM Recipe WHERE NAME LIKE "Sweet and Sour Pork"),
+    (SELECT INGREDIENT_ID FROM Ingredient WHERE NAME LIKE 'Pork, lean'),
+    (SELECT RECIPE_ID FROM Recipe WHERE NAME LIKE 'Sweet and Sour Pork'),
     100
 ),
 (
-    (SELECT INGREDIENT_ID FROM Ingredient WHERE NAME LIKE "Chicken, breast"),
-    (SELECT RECIPE_ID FROM Recipe WHERE NAME LIKE "Chicken Quesidillas"),
+    (SELECT INGREDIENT_ID FROM Ingredient WHERE NAME LIKE 'Chicken, breast'),
+    (SELECT RECIPE_ID FROM Recipe WHERE NAME LIKE 'Chicken Quesidillas'),
     100
 ),
 (
-    (SELECT INGREDIENT_ID FROM Ingredient WHERE NAME LIKE "Milk Chocolate"),
-    (SELECT RECIPE_ID FROM Recipe WHERE NAME LIKE "Chocolate Cake"),
+    (SELECT INGREDIENT_ID FROM Ingredient WHERE NAME LIKE 'Milk Chocolate'),
+    (SELECT RECIPE_ID FROM Recipe WHERE NAME LIKE 'Chocolate Cake'),
     100
 );

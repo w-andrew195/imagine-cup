2dd97c2ed0cafd47ee067032d0e36f0d5797d8c1
rcui761 <ambercui199528@gmail.com>
2018-03-24 22:31:27 +1300

    Edit the CookSkillScreen

--- reactApp/src/screens/profile/profile-cooking-skill-screen.js
+++ reactApp/src/screens/profile/profile-cooking-skill-screen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, CheckBox } from 'react-native';
 import StarRating from 'react-native-star-rating';
 
 class CookSkillScreen extends Component {
@@ -18,27 +18,57 @@ class CookSkillScreen extends Component {
 
     render() {
         return (
-                <View style={{ alignItems: 'center', width: 300, margin: 'auto', marginTop: 40}}>
-                        <Text style={{
-                            fontSize: 16,
-                            marginBottom: 10,
-                            fontFamily: "sans-serif",
-                        }}>My Cooking Skill:</Text>
-                        <StarRating
-                            disabled={false}
-                            maxStars={5}
-                            rating={this.state.starCount}
-                            selectedStar={(rating) => this.onStarRatingPress(rating)}
-                            fullStarColor={"hsl(88,  50%, 53%)"}
+            <View style={{ alignItems: 'center', width: 300, margin: 'auto', marginTop: 40 }}>
+                <Text style={{
+                    fontSize: 16,
+                    marginBottom: 10,
+                    fontFamily: "sans-serif",
+                }}>My Cooking Skill:</Text>
+                <StarRating
+                    disabled={false}
+                    maxStars={5}
+                    rating={this.state.starCount}
+                    selectedStar={(rating) => this.onStarRatingPress(rating)}
+                    fullStarColor={"hsl(88,  50%, 53%)"}
+                />
+                <Text style={{
+                    fontSize: 16,
+                    marginBottom: 10,
+                    fontFamily: "sans-serif",
+                    marginTop: 20
+                }}>My Favourate Food:</Text>
+                <View style={{ flexDirection: 'column' }}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <CheckBox
+                            value={this.state.checked}
+                            onValueChange={() => this.setState({ checked: !this.state.checked })}
                         />
-                        <Text style={{
-                            fontSize: 16,
-                            marginBottom: 10,
-                            fontFamily: "sans-serif",
-                            marginTop: 20
-                        }}>My Favourate Food:</Text>
-                  
+                        <Text style={{ marginTop: 5 }}> Kiwi Food </Text>
+                    </View>
+                    <View style={{ flexDirection: 'row' }}>
+                        <CheckBox
+                            value={this.state.checked2}
+                            onValueChange={() => this.setState({ checked2: !this.state.checked2 })}
+                        />
+                        <Text style={{ marginTop: 5 }}> American Food </Text>
+                    </View>
+                    <View style={{ flexDirection: 'row' }}>
+                        <CheckBox
+                            value={this.state.checked3}
+                            onValueChange={() => this.setState({ checked3: !this.state.checked3 })}
+                        />
+                        <Text style={{ marginTop: 5 }}> Asian Food </Text>
+                    </View>
+                    <View style={{ flexDirection: 'row' }}>
+                        <CheckBox
+                            value={this.state.checked4}
+                            onValueChange={() => this.setState({ checked4: !this.state.checked4 })}
+                        />
+                        <Text style={{ marginTop: 5 }}> Indian Food </Text>
+                    </View>
                 </View>
+
+            </View>
 
         );
     }
